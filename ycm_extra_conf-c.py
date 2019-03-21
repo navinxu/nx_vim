c4644110766a3e13528bafe373a08158655306a2
许叶杨 <xuyeyang@outlook.com>
2019-03-21 21:23:52 +0800

    让C++的头文件补全可用

--- ycm_extra_conf-c.py
+++ ycm_extra_conf-c.py
@@ -36,61 +36,68 @@ import ycm_core
 
 DIR_OF_THIS_SCRIPT = os.path.abspath( os.path.dirname( __file__ ) )
 DIR_OF_THIRD_PARTY = os.path.join( DIR_OF_THIS_SCRIPT, 'third_party' )
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
+SOURCE_EXTENSIONS = [ '.cpp', '.h', '.cxx', '.cc', '.c', '.m', '.mm' ]
 
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER and/or -DYCM_EXPORT in your flags;
-# only the YCM source code needs it.
-'-DUSE_CLANG_COMPLETER',
-'-DYCM_EXPORT=',
-# THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
-# use when compiling headers. So it will guess. Badly. So C++ headers will be
-# compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
-# For a C project, you would set this to 'c' instead of 'c++'.
-#'-x',
-#'c++',
-'-std=c99',
-'-x', 'c',
-'-isystem',
-'/usr/include/',
-'-isystem',
-'/usr/lib/gcc/x86_64-linux-gnu/7/include/'
-'-isystem',
-'cpp/pybind11',
-'-isystem',
-'cpp/BoostParts',
-'-isystem',
-get_python_inc(),
-'-isystem',
-'cpp/llvm/include',
-'-isystem',
-'cpp/llvm/tools/clang/include',
-'-I',
-'cpp/ycm',
-'-I',
-'cpp/ycm/ClangCompleter',
-'-isystem',
-'cpp/ycm/tests/gmock/gtest',
-'-isystem',
-'cpp/ycm/tests/gmock/gtest/include',
-'-isystem',
-'cpp/ycm/tests/gmock',
-'-isystem',
-'cpp/ycm/tests/gmock/include',
-'-isystem',
-'cpp/ycm/benchmarks/benchmark/include',
-'-I',
-'/usr/i686-w64-mingw32/include/conio.h',
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DNDEBUG',
+    # You 100% do NOT need -DUSE_CLANG_COMPLETER and/or
+    # DYCM_EXPORT in your flags;
+    # only the YCM source code needs it.
+    '-DUSE_CLANG_COMPLETER',
+    '-DYCM_EXPORT=',
+    # THIS IS IMPORTANT! Without the '-x' flag,
+    # Clang won't know which language to
+    # use when compiling headers. So it will guess.
+    # Badly. So C++ headers will be
+    # compiled as C headers. You don't want that so
+    # ALWAYS specify the '-x' flag.
+    # For a C project, you would set this to 'c' instead of 'c++'.
+    # '-x',
+    # 'c++',
+    # '-std=c99',
+    '-std=c11',
+    '-x', 'c',
+    '-isystem',
+    '/usr/include/',
+    '-isystem',
+    '/usr/lib/gcc/x86_64-linux-gnu/8/include/'
+    '-isystem',
+    '/usr/include/',
+    '-isystem',
+    'cpp/pybind11',
+    '-isystem',
+    'cpp/BoostParts',
+    '-isystem',
+    get_python_inc(),
+    '-isystem',
+    'cpp/llvm/include',
+    '-isystem',
+    'cpp/llvm/tools/clang/include',
+    '-I',
+    'cpp/ycm',
+    '-I',
+    'cpp/ycm/ClangCompleter',
+    '-isystem',
+    'cpp/ycm/tests/gmock/gtest',
+    '-isystem',
+    'cpp/ycm/tests/gmock/gtest/include',
+    '-isystem',
+    'cpp/ycm/tests/gmock',
+    '-isystem',
+    'cpp/ycm/tests/gmock/include',
+    '-isystem',
+    'cpp/ycm/benchmarks/benchmark/include',
+    '-I',
+    '/usr/i686-w64-mingw32/include/conio.h',
 ]
 
 # Clang automatically sets the '-std=' flag to 'c++14' for MSVC 2015 or later,
